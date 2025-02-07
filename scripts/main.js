b520f8fe9941d35d2f0f5ac3b4ab9a7ccbc6ce51
nasafrasa <nasafrasa.mail@gmail.com>
2025-02-07 00:20:17 -0800

    Initializing...

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,7 @@
 const filename = window.location.pathname.split("/").pop();
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(filename)
-    if (filename.includes("index")) {
+    if (filename.includes("index") || !filename) {
         fetch("https://nasafrasa.github.io/directories/indexsidebar.html")
         .then(response => response.text())
         .then(data => document.getElementById("sidebar-container").innerHTML = data)
