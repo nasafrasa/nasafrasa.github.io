1048b3823bcf5d8fb5a6ecdf42375442379c1e71
nasafrasa <nasafrasa.mail@gmail.com>
2025-02-07 00:07:44 -0800

    Initializing...

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,15 @@
 const filename = window.location.pathname.split("/").pop();
 
-if (filename.includes("index")) {
-    fetch("https://nasafrasa.github.io/directories/indexsidebar.html")
-    .then(response => response.text())
-    .then(data => document.getElementById("sidebar-container").innerHTML = data)
-    .catch(error => console.error("Error loading sidebar:", error));
-} else {
-    fetch("https://nasafrasa.github.io/directories/sidebar.html")
-    .then(response => response.text())
-    .then(data => document.getElementById("sidebar-container").innerHTML = data)
-    .catch(error => console.error("Error loading sidebar:", error));
-}
+document.addEventListener("DOMContentLoaded", () => {
+    if (filename.includes("index")) {
+        fetch("https://nasafrasa.github.io/directories/indexsidebar.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("sidebar-container").innerHTML = data)
+        .catch(error => console.error("Error loading sidebar:", error));
+    } else {
+        fetch("https://nasafrasa.github.io/directories/sidebar.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("sidebar-container").innerHTML = data)
+        .catch(error => console.error("Error loading sidebar:", error));
+    }
+})
