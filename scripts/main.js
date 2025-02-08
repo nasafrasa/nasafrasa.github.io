c1461a2984ca88097a3e0527bb575cbfe7a14d38
nasafrasa <nasafrasa.mail@gmail.com>
2025-02-07 17:55:59 -0800

    Initializing...

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,45 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(response => response.text())
         .then(data => document.getElementById("sidebar-container").innerHTML = data)
         .catch(error => console.error("Error loading sidebar:", error));
+
+
+    const filename = window.location.pathname.split("/").pop().split(".html")[0];
+    const gallery = document.querySelector(".gallery");
+
+    const imageFolder = "../images/" + filename + "/";
+    let imageList
+
+    if (filename == "cats") {
+        imageList = [
+            ["image1.png", ""],
+            ["image2.png", ""],
+            ["image3.png", ""],
+            ["image4.png", ""],
+            ["image5.png", ""],
+            ["image6.png", ""],
+            ["image7.png", ""],
+            ["image8.png", ""],
+            ["image9.png", ""],
+            ["image10.png", ""],
+            ["image11.png", ""],
+            ["image12.png", ""],
+            ["image13.png", ""],
+            ["image14.png", ""],
+            ["image15.png", ""],
+            ["image16.png", ""],
+            ["image17.png", ""],
+            ["image18.png", ""],
+            ["image19.png", ""],
+            ["image20.png", ""],
+            ["image21.png", ""],
+            ["image22.png", ""],
+            ["image23.png", ""]
+        ];
+    }
+
+    imageList.forEach(image => {
+        const img = document.createElement("img");
+        img.src = imageFolder + image[0];
+        gallery.appendChild(img);
+    });
 })
